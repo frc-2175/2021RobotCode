974802ad90188d16368f2fbb3a3b5477ffa7047c
Ben Visness <bvisness@gmail.com>
2020-12-31 13:22:46 -0600

    Allow robot to start with max air

--- Robot.gd
+++ Robot.gd
@@ -2,25 +2,29 @@ extends Spatial
 
 class_name Robot
 
-var system_air_volume_cm3
-var extra_air_volume_cm3 = 0
+export(bool) var start_with_max_air = false
+
+var system_air_volume_cm3: float
+var extra_air_volume_cm3: float = 0.0
 
 var atmospheric_pressure_Npcm2 = 10.13
 
 func _ready():
 	system_air_volume_cm3 = get_system_air_volume_cm3()
+	if start_with_max_air:
+		extra_air_volume_cm3 = pressure_Npcm2_to_extra_air_volume_cm3(Math.psi2Npcm2(120) + atmospheric_pressure_Npcm2)
 
-func get_volume_scale():
-	if system_air_volume_cm3 == 0:
+func get_volume_scale(extra_cm3: float = extra_air_volume_cm3):
+	if system_air_volume_cm3 == 0 or extra_cm3 == 0:
 		return 1.0
 
-	return system_air_volume_cm3 / (system_air_volume_cm3 + extra_air_volume_cm3)
+	return system_air_volume_cm3 / (system_air_volume_cm3 + extra_cm3)
 
-func get_air_pressure_Npcm2() -> float:
+func get_air_pressure_Npcm2(extra_cm3: float = extra_air_volume_cm3) -> float:
 	if system_air_volume_cm3 == 0:
 		return 0.0
 
-	return atmospheric_pressure_Npcm2 / get_volume_scale()
+	return atmospheric_pressure_Npcm2 / get_volume_scale(extra_cm3)
 
 func get_working_pressure_Npcm2() -> float:
 	return min(Math.psi2Npcm2(60), get_air_pressure_Npcm2())
@@ -36,6 +40,10 @@ func get_system_air_volume_cm3(node: Node = null) -> float:
 		total += get_system_air_volume_cm3(child)
 	
 	return total
+
+# I dunno man, I just did some algebra and this came out
+func pressure_Npcm2_to_extra_air_volume_cm3(pressure_Npcm2: float) -> float:
+	return ((pressure_Npcm2 * system_air_volume_cm3) / atmospheric_pressure_Npcm2) - system_air_volume_cm3
 
 # awkwardly fit to this data:
 # https://www.andymark.com/products/air-compressor?via=Z2lkOi8vYW5keW1hcmsvV29ya2FyZWE6Ok5hdmlnYXRpb246OlNlYXJjaFJlc3VsdHMvJTdCJTIycSUyMiUzQSUyMmNvbXByZXNzb3IlMjIlN0Q
